59cf4626475001f3157dcb75932eb1294141e920
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-11-20 09:53:59 -0600

    Mostly done

--- w5d1_/411_wk5_day1_mapstatetoprops/src/components/Car.js
+++ w5d1_/411_wk5_day1_mapstatetoprops/src/components/Car.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Container, Paper, Chip } from '@material-ui/core';
-import cars from '../cars.json' // remove this
+// import cars from '../cars.json' // remove this
 
 const Car = (props) => {
     const id = props.match.params.id
-    {/* Change cars to props.cars and remove the cars.json import above */}
-    const car = cars.find(c => c.id == id)
+    // Change cars to props.cars and remove the cars.json import above
+	const car = props.cars.find(c => c.id == id)
 
     return (
         <Container maxWidth="sm" className="car-container">
@@ -13,7 +13,7 @@ const Car = (props) => {
                 <h2>{car.Name}</h2>
                 {
                     Object.keys(car).map((key, idx) => {
-                        return <Chip label={`${key}: ${car[key]}`}></Chip>
+                        return <Chip key={idx} label={`${key}: ${car[key]}`}></Chip>
                     })
                 }
             </Paper>
