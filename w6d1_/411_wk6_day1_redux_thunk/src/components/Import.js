f36d873f6c4d9880369971c22fef78d9fd43fbc1
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-12-09 09:09:53 -0600

    thunk is done

--- w6d1_/411_wk6_day1_redux_thunk/src/components/Import.js
+++ w6d1_/411_wk6_day1_redux_thunk/src/components/Import.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import {
+    Container,
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+    TableCell,
+	Button
+} from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const Import = (props) => {
-    // fill out this component
-
     return (
-        <p>Import Component</p>
+		<Container>
+			<Button
+				variant="contained"
+				color="primary"
+				onClick={props.fetchMakes}
+			>Import</Button>
+			<Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {props.makes.map( ( make, index ) => (
+                    <TableRow key={make["MakeId"]}>
+                        <TableCell>
+                            {make["MakeId"]}
+                        </TableCell>
+                        <TableCell>
+							{make["MakeName"]}
+						</TableCell>
+                        <TableCell>
+							<DeleteIcon
+                                onClick={() => props.deleteMake(index)}
+                                className="icon text-red" />
+                        </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+			</Table>
+		</Container>
     )
 }
 
