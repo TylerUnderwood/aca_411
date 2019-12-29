c11c3b52fe5c8ab4115a4df7294c7cc76819fa52
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-12-28 18:53:01 -0600

    Added maps stuff

--- w7d1_/small-buisness/src/components/ListingEntry.js
+++ w7d1_/small-buisness/src/components/ListingEntry.js
@@ -3,12 +3,22 @@ import {
 	Container, 
 	Paper,
 } from '@material-ui/core';
+import GoogleMapReact from "google-map-react";
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>
 
 const ListingEntry = ( props ) => {
 
 	const id = props.match.params.id
 	const listing = props.business.find( item => item.id == id )
+
+	const defaultProps = {
+		center: {
+			lat: Number(listing.latitude),
+			lng: Number(listing.longitude),
+		},
+		zoom: 13
+	};
 	
     return (
 		<Container maxWidth="sm" className="listing-container pad">
@@ -18,6 +28,16 @@ const ListingEntry = ( props ) => {
 				<span>{listing.hours}</span>
 				<p>{listing.description}</p>
             </Paper>
+			<Paper className="listing-paper" style={{ height: "450px" }}>
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY }}
+					defaultCenter={defaultProps.center}
+					defaultZoom={defaultProps.zoom}
+					yesIWantToUseGoogleMapApiInternals
+				>
+				<AnyReactComponent lat={listing.latitude} lng={listing.longitude} text={listing.name} />
+				</GoogleMapReact>
+			</Paper>
         </Container>
     )
 }
--- w7d1_/small-buisness/src/redux/state.js
+++ w7d1_/small-buisness/src/redux/state.js
@@ -9,35 +9,45 @@ export default {
 			name: "Dragon's Lair Comics & Fantasy",
 			description: "Spacious store stocking comics, costumes, games & gifts with tables for play & special-event nights.",
 			hours: "11AM–12AM",
-			address: "2438 W Anderson Ln B1, Austin, TX 78757"
+			address: "2438 W Anderson Ln B1, Austin, TX 78757",
+			latitude: "30.318718",
+			longitude: "-97.739047",
 		},
 		{
 			id: 2,
 			name: "Austin Books & Comics",
 			description: "Longtime shop featuring an array of comic books, plus graphic novels & pop culture paraphernalia.",
 			hours: "11AM–7PM",
-			address: "5002 N Lamar Blvd, Austin, TX 78751"
+			address: "5002 N Lamar Blvd, Austin, TX 78751",
+			latitude: "30.318718",
+			longitude: "-97.739047",
 		},
 		{
 			id: 3,
 			name: "Turquoise Trading Post",
 			description: "Large retail spot specializing in Native American jewelry & Southwestern pottery, crafts & gifts.",
 			hours: "10AM–6:30PM",
-			address: "6103 Burnet Rd, Austin, TX 78757"
+			address: "6103 Burnet Rd, Austin, TX 78757",
+			latitude: "30.318718",
+			longitude: "-97.739047",
 		},
 		{
 			id: 4,
 			name: "End of An Ear",
 			description: "Cozy record store stocking a diverse array of new, vintage & rare vinyl records, plus CDs & DVDs.",
 			hours: "11AM–9PM",
-			address: "4304 Clawson Rd, Austin, TX 78704"
+			address: "4304 Clawson Rd, Austin, TX 78704",
+			latitude: "30.318718",
+			longitude: "-97.739047",
 		},
 		{
 			id: 5,
 			name: "Bouldin Creek Cafe",
 			description: "Eco-friendly, bohemian cafe offering hearty meatless meals, coffees & a rotating monthly art show.",
 			hours: "7AM–12AM",
-			address: "1900 S 1st St, Austin, TX 78704"
+			address: "1900 S 1st St, Austin, TX 78704",
+			latitude: "30.318718",
+			longitude: "-97.739047",
 		}
 	]
 };
